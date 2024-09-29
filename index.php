bf5848ae0252c47cd781a3586a9620aec3cf1822
YouraNoAI <154102639+YouraNoAI@users.noreply.github.com>
2024-09-29 16:23:07 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Perpustakaan Senja</title>
+</head>
+<body>
+
+    <header>
+        <h1><i class="fa-solid fa-book-open-reader">
+            NAI STORE
+        </i></h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="koleksi.php">Spesial</a></li>
+            <li><a href="kontak.php">Kontak</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+    <div class="container">
+
+        <div class="library">
+            <h2>Selamat Datang di NAI STORE</h2>
+            <p>Flash Sell !!!</p>
+        </div>
+
+        <div class="pos">
+            <div class="card">
+                <div class="card-image">
+                    <img src="IMG/bumi.jpg" alt="Buku_Hoshi">
+                </div>
+                <div class="card-detail">
+                    <h3 class="book-title">BUMI</h3>
+                    <p class="book-details">Penulis : Tere Liye</p>
+                    <p class="book-details">Tahun Terbit: 2020</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-image">
+                    <img src="IMG/bulan.jpg" alt="Buku_Hoshi">
+                </div>
+                <div class="card-detail">
+                    <h3 class="book-title">BULAN</h3>
+                    <p class="book-details">Penulis : Tere Liye</p>
+                    <p class="book-details">Tahun Terbit: 2020</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="card-image">
+                    <img src="IMG/bintang.jpg" alt="Buku_Hoshi">
+                </div>
+                <div class="card-detail">
+                    <h3 class="book-title">BINTANG</h3>
+                    <p class="book-details">Penulis : Tere Liye</p>
+                    <p class="book-details">Tahun Terbit: 2020</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+        &copy; Hak Cipta Kelas_Senja
+    </footer>
+
+</body>
+</html>
